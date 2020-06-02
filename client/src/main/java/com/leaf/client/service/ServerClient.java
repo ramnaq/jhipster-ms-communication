5b6cd34a7b1115b6795ca4ad0d0000fe9748b109
Ramna Sidharta <ramna.sidharta@leafagriculture.com.br>
2020-06-02 16:03:32 -0300

    Remove unnecessary comment from ServerClient.java

--- client/src/main/java/com/leaf/client/service/ServerClient.java
+++ client/src/main/java/com/leaf/client/service/ServerClient.java
@@ -5,15 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-/**
- * name = the name of the service to be called, resolve by Eureka, used by the client-side LoadBalancer
- * url = overrides the default (requires 'name')
- * decode404 = fallback for mapping 404 to null
- * qualifier = changes the name of the Bean, which is the interface name by default
- * configuration = overrides the default feign configuration (which is config.FeignConfiguration). Can be used for things
- *      like providing a BasicAuthRequestInterceptor bean, with specific "user" and "password".
- * contextId = used differ FeignClients with different configurations, but that calls the same service
- */
 @FeignClient(name = "server", url = "http://localhost:8020")
 public interface ServerClient {
 
